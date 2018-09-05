80927b6c5e2cb1ed0c337ea889cafa462964d654
RMhanovernorwichschools <37371520+RMhanovernorwichschools@users.noreply.github.com>
2018-09-05 18:04:34 -0400

    Updated from Brython Server: 9/5/2018 6:04:32 PM

--- stringjumble.py
+++ stringjumble.py
@@ -46,5 +46,18 @@ space_loci=[]
 for x in range (0,sl):
     if characters[x]==' ':
         space_loci.append(x)
-    
-print(space_loci)
+
+reverse_space_loci=space_loci[len(space_loci):0:-1]
+reverse_space_loci.append(space_loci[0])
+
+fin=len(characters)
+for x in reverse_space_loci:
+    start=x
+    for n in range(start,fin):
+        print_b=print_b+characters[n]
+    fin=x
+    print_b=print_b+' '
+for n in range(0,fin+1):
+    print_b=print_b+characters[n]
+
+print(print_b)
